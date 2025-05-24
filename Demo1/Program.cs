37ee569c68ae71ca9425339c98aa4d61eb0ee23b
jorgemagic <jferrero@plainconcepts.com>
2025-05-24 09:39:19 +0200

    - Added more examples

--- Demo1/Program.cs
+++ Demo1/Program.cs
@@ -1,6 +1,8 @@
 ﻿using CSnakes.Runtime;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Runtime.InteropServices;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Demo1
 {
@@ -39,16 +41,52 @@ namespace Demo1
 
             var module = environment.Demo1();
 
-            // Call Function 1
+            // Hello world
             var result = module.HelloWorld(".NET");
             Console.WriteLine(result);
 
-            // Call Function 2
+            // Classes
             var person = module.CreatePerson("John", 41);
             string name = person.GetAttr("name").ToString();
             int.TryParse(person.GetAttr("age").ToString(), out int age);
             Console.WriteLine($"Person Name:{name} Age:{age}");
 
-        }
+            // Async function
+            Task.Run(async () =>
+            {
+                var number = await module.AsyncFunction();
+                Console.WriteLine($"Async function return {number}");
+            });
+
+            // Crossing the bridge too frequently
+            var names = module.MakeSquare2dArray(1000);
+
+            for (int i = 0; i < 1000; i++)
+            {
+                for (int j = 0; j < 1000; j++)
+                {
+                    module.SetRandom(names, i, j);
+                }
+            }
+            Console.WriteLine($"Crossing the bridge too frequently");
+
+            // Marshalling return values unnecessarily
+            module.LogValue("Add this message to the logger");
+
+            // Passing a large amount of data
+            double[] data = new double[1000];
+            var random = new Random();
+            for (int i = 0;i < 1000;i++)
+            {
+                data[i] = random.NextDouble();
+            }
+            IReadOnlyList<double> dataAsList = data.ToList();
+            module.GetData1(dataAsList);
+
+            byte[] dataAsBytes = MemoryMarshal.AsBytes<double>(data.AsSpan()).ToArray();
+            module.GetData2(dataAsBytes);
+
+            Console.ReadLine();
+        }        
     }
 }
--- Demo1/Python/demo1.py
+++ Demo1/Python/demo1.py
@@ -1,6 +1,8 @@
-﻿def hello_world(name: str) -> str:
+﻿# Hello world
+def hello_world(name: str) -> str:
     return f"Hello, {name}!"
 
+# Classes
 class Person:
     def __init__(self, name: str, age: int):
         self.name = name
@@ -8,3 +10,35 @@ class Person:
 
 def create_person(name: str, age: int) -> Person:
     return Person(name, age)
+
+# Async support
+import asyncio
+from typing import Coroutine
+
+async def async_function() -> Coroutine[int, None, None]:
+    await asyncio.sleep(1)
+    return 42
+
+# Crossing the bridge too frenquently
+import numpy as np
+
+def make_square_2d_array(n: int) -> np.ndarray:
+    return np.zeros((n, n))
+
+def set_random(arr, i: int, j: int) -> None:
+    arr[i, j] = np.random.random()
+
+# Marshalling return values unnecessarily
+from typing import Any
+def log_value(value: str) -> Any:
+    print(f"Received value: {value}")
+    return 1;
+
+# Passing a large amount of data
+def get_data1(data: list[float]) -> None:
+    return
+
+def get_data2(data: bytes) -> None:
+    return;
+    
+
