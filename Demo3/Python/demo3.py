d6597f96f4fa93d349d4012080b553703fafbb35
jorgemagic <jferrero@plainconcepts.com>
2025-05-29 11:35:33 +0200

    - Minor changes

--- Demo3/Python/demo3.py
+++ Demo3/Python/demo3.py
@@ -1,14 +1,21 @@
 ﻿import numpy as np
 import cv2
 import torch
-from segment_anything import sam_model_registry, SamPredictor, SamAutomaticMaskGenerator
+from segment_anything import sam_model_registry, SamAutomaticMaskGenerator
 
 # Load model only a single time
 sam = sam_model_registry["vit_b"](checkpoint="Python/sam_vit_b_01ec64.pth")
-mask_generator = SamAutomaticMaskGenerator(sam)
+mask_generator = SamAutomaticMaskGenerator(
+    sam,
+    points_per_side=16,            # ↓ de 32→16 (256 points)
+    pred_iou_thresh=0.88,          
+    stability_score_thresh=0.92,   
+    box_nms_thresh=0.7,            
+    min_mask_region_area=100       
+)
 
 def segment_image(image_path: str) -> list[list[int]]:
-     # 1) lee y valida
+     # 1) Read image
     image = cv2.imread(image_path)
     if image is None:
         raise FileNotFoundError(image_path)
